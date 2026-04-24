c8312655abb99266a9ee1897b72aafe082ff4707
Aleksi Backman <bddubbb@protonmail.com>
2026-04-24 16:02:22 +0300

    Announce stopping of KVM kernel modules

--- user_account/scripts/launch/launch_virtualbox.sh
+++ user_account/scripts/launch/launch_virtualbox.sh
@@ -23,6 +23,10 @@ main() {
 	echo "Mounting disk that is used for storing virtual machines."
 	sudo mount UUID="${target_disk_uuid}" /mnt/temp
 
+	# VirtualBox fails to start virtual machines if the kvm kernel modules
+	# are running. Trying to start results in error: "AMD-V is being used
+	# by another hypervisor (VERR_SVM_IN_USE).".
+	echo "Stopping KVM kernel modules."
 	sudo modprobe -r "${cpu_specific_kvm_module}"
 	sudo modprobe -r kvm
 
