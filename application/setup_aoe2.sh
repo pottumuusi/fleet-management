d9c65072edaf99fb1a99ce5c30a7e0f15abb7067
Aleksi Backman <bddubbb@protonmail.com>
2026-07-03 02:02:10 +0300

    Conditionally print instructions
    Additionally tweak prints.

--- application/setup_aoe2.sh
+++ application/setup_aoe2.sh
@@ -13,6 +13,7 @@ set -e
 readonly temporary_directory="$(mktemp -d)"
 readonly url_vc_redist='https://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x64.exe'
 readonly dir_of_ucrtbase="${HOME}/.steam/steam/steamapps/compatdata/813780/pfx/drive_c/windows/system32"
+readonly arg_manual_setup_done='--manual-setup-done'
 
 error_exit() {
 	echo "${1}"
@@ -20,11 +21,16 @@ error_exit() {
 }
 
 main() {
+	echo "================================="
+	echo "Start of manual part of the setup"
+	echo "================================="
+	echo ""
 	echo "1. Navigate to: Steam -> Settings -> Compatibility"
 	echo "2. Select at least Proton 8.0-4"
 	echo ""
 	echo "If lacking Vulkan support, try using OpenGL via WineD3D."
-	echo "Set \`PROTON_USE_WINED3D=1 %command%\` in launch options to use WineD3D."
+	echo "Set \`PROTON_USE_WINED3D=1 %command%\` in launch options to use"
+	echo "WineD3D."
 	echo ""
 	echo "Install the game via Steam."
 	echo ""
@@ -32,6 +38,18 @@ main() {
 	echo ""
 	echo "Start the game once."
 	echo ""
+	echo "==============================="
+	echo "End of manual part of the setup"
+	echo "==============================="
+
+	if [ "${1}" != "${arg_manual_setup_done}" ] ; then
+		echo ""
+		echo "To run the automated part of the setup, please run the"
+		echo "script with '${arg_manual_setup_done}' as the first"
+		echo "argument. Like so: ${0} ${arg_manual_setup_done}"
+		echo ""
+		exit
+	fi
 
 	if [ ! -d "${temporary_directory}" ] ; then
 		error_exit "Missing temporary directory"
